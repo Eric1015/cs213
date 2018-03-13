ddc4a57726210ce105ee82a9bfe84e26349e0bb5
love1 <love1dbsb77@hotmail.com>
2018-03-13 15:53:13 -0700

    q2.c works properly, needs to check with server

--- code_8/code/q2/q2.c
+++ code_8/code/q2/q2.c
@@ -1,3 +1,6 @@
+#include "stdlib.h"
+#include "stdio.h"
+
 int q2(int a1, int a2, int a3) {
 	int i = 0;
 	switch (a1) {
@@ -9,21 +12,21 @@ int q2(int a1, int a2, int a3) {
 		break;
 	case 14:
 		if (a2 > a3)
-			i = 0;
-		else
 			i = 1;
+		else
+			i = 0;
 		break;
 	case 16:
 		if (a3 > a2)
-			i = 0;
-		else
 			i = 1;
+		else
+			i = 0;
 		break;
 	case 18:
 		if (a2 == a3)
-			i = 0;
-		else
 			i = 1;
+		else
+			i = 0;
 		break;
 	default:
 		i = 0;
@@ -32,6 +35,10 @@ int q2(int a1, int a2, int a3) {
 	return i;
 }
 
-int main(void) {
-	q2(0, 0, 0);
+int main(int argc, char** argv) {
+	int num[3];
+	for (unsigned i = 0; i < sizeof(num) / sizeof(int); i++)
+		num[i] = atoi(argv[i + 1]);
+	int result = q2(num[0], num[1], num[2]);
+	printf("%d\n", result);
 }
