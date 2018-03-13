8999e4fa23ecf485e93a665133530df52830a115
love1 <love1dbsb77@hotmail.com>
2018-03-13 01:27:02 -0700

    q2.c completed

---
+++ code_8/code/q2/q2.c
@@ -0,0 +1,37 @@
+int foo(int a1, int a2, int a3) {
+	int i = 0;
+	switch (a1) {
+	case 10: 
+		i = a2 + a3;
+		break;
+	case 12:
+		i = a2 - a3;
+		break;
+	case 14:
+		if (a2 > a3)
+			i = 0;
+		else
+			i = 1;
+		break;
+	case 16:
+		if (a3 > a2)
+			i = 0;
+		else
+			i = 1;
+		break;
+	case 18:
+		if (a2 == a3)
+			i = 0;
+		else
+			i = 1;
+		break;
+	default:
+		i = 0;
+	}
+
+	return i;
+}
+
+int main(void) {
+
+}
